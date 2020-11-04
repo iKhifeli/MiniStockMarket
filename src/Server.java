c0fc0cc5b604ce337258e093be815ab26b7945ee
Avramesc Marian <marian_avramesc03@yahoo.com>
2020-11-04 13:59:10 +0200

    removed redundant casts

--- src/Server.java
+++ src/Server.java
@@ -9,20 +9,20 @@ public class Server {
 
     public void operate(Client client){
         if(client.isBuyer){
-            System.out.println("The client " + ((Buyer)client).getName() + " is a buyer");
-            if(((Buyer)client).wantsToTrade()){
-                System.out.println("The buyer " + ((Buyer)client).getName() + " is looking for " + ((Buyer) client).getWantedOffer() + " offer");
+            System.out.println("The client " + client.getName() + " is a buyer");
+            if(client.wantsToTrade()){
+                System.out.println("The buyer " + client.getName() + " is looking for " + ((Buyer) client).getWantedOffer() + " offer");
                 if(db.buyOffer(client, ((Buyer) client).getWantedOffer(), ((Buyer) client).getWantedQuantity())){ // this is SUBJECT TO CHANGE
-                    ((Buyer)client).doesNotWantToTradeAnymore();
-                    System.out.println("The buyer " + ((Buyer)client).getName() + " has bought an offer and does not want to trade anymore");
+                    client.doesNotWantToTradeAnymore();
+                    System.out.println("The buyer " + client.getName() + " has bought an offer and does not want to trade anymore");
                 }
             }
         } else {
             System.out.println("The client is a seller");
-            if(((Seller)client).wantsToTrade()) {
+            if(client.wantsToTrade()) {
                 System.out.println("The seller wants to list his offer");
                 if(db.listNewOffer(((Seller) client).getOffer())){
-                    ((Seller)client).doesNotWantToTradeAnymore();
+                    client.doesNotWantToTradeAnymore();
                     return;
                 }
                 System.out.println("The offer already exists");
